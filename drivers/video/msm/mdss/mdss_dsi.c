2d8f3ebfd9689b3359a0d239a01974f5e06395d8
Pranav Vashi <neobuddy89@gmail.com>
2015-04-02 01:08:29 -0700

    drivers: Add state notifier driver
    * To be used by multiple drivers to function suspend/resume
      routines.
    * For hammerhead, it is similar to LCD notifier driver.
    * Provides a toggle to use FB notifier which is useful for
      ambient mode display users.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Zachariah <zkennedy87@gmail.com>
    
    Conflicts:
    	drivers/input/touchscreen/Makefile
    	drivers/video/msm/mdss/mdss_dsi.c
    
    Change-Id: Ic6c5c40473c06243aace9624fc19cf38423ed58f

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -23,6 +23,10 @@
 #include <linux/regulator/consumer.h>
 #include <linux/lcd_notify.h>
 
+#ifdef CONFIG_STATE_NOTIFIER
+#include <linux/state_notifier.h>
+#endif
+
 #include "mdss.h"
 #include "mdss_panel.h"
 #include "mdss_dsi.h"
@@ -1063,7 +1067,14 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		ctrl_pdata->ctrl_state |= CTRL_STATE_MDP_ACTIVE;
 		if (ctrl_pdata->on_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_unblank(pdata);
+
 		lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
+		state_suspended = false;
+
+#ifdef CONFIG_STATE_NOTIFIER
+		if (!use_fb_notifier)
+			lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
+#endif
 		break;
 	case MDSS_EVENT_BLANK:
 		lcd_notifier_call_chain(LCD_EVENT_OFF_START, NULL);
@@ -1076,6 +1087,12 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 			rc = mdss_dsi_blank(pdata);
 		rc = mdss_dsi_off(pdata);
 		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
+		state_suspended = true;
+
+#ifdef CONFIG_STATE_NOTIFIER
+		if (!use_fb_notifier)
+			lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
+#endif
 		break;
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
 	/* 2013-10-18 added begin for continous splash */
